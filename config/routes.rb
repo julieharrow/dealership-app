6d6eed7967deb30f98447e66d356955d1ba59582
Juile Harrow <julie.ann.harrow@gmail.com>
2016-07-19 14:20:33 -0400

    Added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :vehicles
   root 'vehicles#index'
   # The priority is based upon order of creation: first created -> highest priority.
